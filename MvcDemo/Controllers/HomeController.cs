bee6f88b820231a34cd7b57a6277f53b6edea4a4
pc <pc@DESKTOP-VLT6RM4>
2021-05-01 14:47:08 +0530

    Add project files.

---
+++ MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string id, string name)
+        {
+            return "Id = " + id + "Name = " + name;
+        }
+
+        public string GetDetails()
+        {
+            return "GetDetails Invoked";
+        }
+
+        public ViewResult New()
+        {
+           ViewBag.Countries =  new List<string>()
+            {
+                "Sri Lanka",
+                "Usa",
+                "India",
+                "Australia"
+            };
+            return View();
+        }
+
+    }
+}
